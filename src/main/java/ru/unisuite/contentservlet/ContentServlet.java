72ec3ca285d045c7fc559080af2ae6651bd4e0fe
Adusya <druna007@rambler.ru>
2019-01-23 19:14:28 +0300

    change url to get and get/secure

--- src/main/java/ru/unisuite/contentservlet/ContentServlet.java
+++ src/main/java/ru/unisuite/contentservlet/ContentServlet.java
@@ -20,7 +20,7 @@ import ru.unisuite.cache.cacheexception.CacheStartFailedException;
 import ru.unisuite.contentservlet.databasereader.DatabaseReaderException;
 import ru.unisuite.contentservlet.databasereader.DatabaseReaderNoDataException;
 
-@WebServlet("/*")//@WebServlet({ "/get/*", "/get/secure/*" })
+@WebServlet({ "/get/*", "/get/secure/*" })
 public class ContentServlet extends HttpServlet {
 
 	private ContentGetter contentGetter;
